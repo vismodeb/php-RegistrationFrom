acdb1e9d5bda526cdc53434f18fdabd2774ea3de
vismo_dev <105719010+vismodeb@users.noreply.github.com>
2023-08-05 11:34:18 +0600

    Update Title

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registration Form</title>
+    <title>Registration Form - Vismo_Dev</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
   </head>
